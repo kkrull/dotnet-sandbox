445546a2f320961025e7579fc24919a89aab2e32
Kyle Krull <kkrull@8thlight.com>
2023-07-25 12:01:54 -0500

    Migrate standalone project to sandbox

---
+++ dotnet-framework-4/WingtipToys/Site.Master.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Web.UI;
+
+namespace WingtipToys
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
