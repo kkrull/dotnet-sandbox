5de3792a9022623d6dbbefc3dab5c0b40c6dc1a4
Kyle Krull <kkrull@8thlight.com>
2023-07-25 14:07:09 -0500

    Uncomment entity type

--- dotnet-framework-4/WingtipToys/Models/WingtipsContext.cs
+++ dotnet-framework-4/WingtipToys/Models/WingtipsContext.cs
@@ -19,13 +19,12 @@ namespace WingtipToys.Models
 
     // Add a DbSet for each entity type that you want to include in your model. For more information 
     // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
-
-    // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    public virtual DbSet<MyEntity> MyEntities { get; set; }
   }
 
-  //public class MyEntity
-  //{
-  //    public int Id { get; set; }
-  //    public string Name { get; set; }
-  //}
+  public class MyEntity
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+  }
 }
