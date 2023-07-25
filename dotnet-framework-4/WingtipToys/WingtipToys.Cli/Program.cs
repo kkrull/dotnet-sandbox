783df0ea4ef005425c0c0cd1537e650fbf824b4c
Kyle Krull <kkrull@8thlight.com>
2023-07-25 15:27:57 -0500

    Run the CLI

--- dotnet-framework-4/WingtipToys/WingtipToys.Cli/Program.cs
+++ dotnet-framework-4/WingtipToys/WingtipToys.Cli/Program.cs
@@ -11,6 +11,8 @@ namespace Wingtip.Cli
     public static void Main(string[] args)
     {
       Console.WriteLine("Hello World!");
+      Console.WriteLine("Press any key to exit...");
+      Console.ReadKey();
     }
   }
 }
