e862240c918d18d05899beda9cc4ee58d2d2b0a2
Kyle Krull <kkrull@8thlight.com>
2023-07-25 17:26:05 -0500

    Set initializer

--- dotnet-framework-4/WingtipToys/WingtipToys.Web/Global.asax.cs
+++ dotnet-framework-4/WingtipToys/WingtipToys.Web/Global.asax.cs
@@ -1,17 +1,21 @@
 using System;
+using System.Data.Entity;
 using System.Web;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WingtipToys.Models;
 
 namespace WingtipToys
 {
-    public class Global : HttpApplication
+  public class Global : HttpApplication
+  {
+    void Application_Start(object sender, EventArgs e)
     {
-        void Application_Start(object sender, EventArgs e)
-        {
-            // Code that runs on application startup
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
+      // Code that runs on application startup
+      RouteConfig.RegisterRoutes(RouteTable.Routes);
+      BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+      Database.SetInitializer(new WingtipToysInitializer());
     }
+  }
 }
