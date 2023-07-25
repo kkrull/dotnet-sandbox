bdea809d89f4a792797268838744435c030c1612
Kyle Krull <kkrull@8thlight.com>
2023-07-25 15:13:54 -0500

    Try like anything to get Visual Studio files out
    and C# files in

---
+++ dotnet-framework-4/WingtipToys/Wingtip.Cli/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wingtip.Cli
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
