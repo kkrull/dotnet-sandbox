4f59e57bbfd7bde1b037fb15bdfaacf80c12856e
Kyle Krull <kkrull@8thlight.com>
2024-02-20 07:34:04 -0600

    New .net 8 project

---
+++ dotnet-8/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ dotnet-8/README.md
@@ -0,0 +1,16 @@
+# Dotnet 8 project
+
+Runs with dotnet 8.0.100
+
+## Build
+
+```shell
+dotnet build
+```
+
+## Run
+
+```shell
+dotnet run
+```
+
