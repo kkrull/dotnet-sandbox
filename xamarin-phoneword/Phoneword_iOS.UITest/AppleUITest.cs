016be79053802090326ab3525bddf35038c5a07d
Kyle Krull <kdkrull@gmail.com>
2024-05-23 14:25:49 -0500

    Migrate from xamarin-sandbox

---
+++ xamarin-phoneword/Phoneword_iOS.UITest/AppleUITest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace Phoneword_iOS.UITest
+{
+    [TestFixture]
+    public class AppleUITest
+    {
+        [Test]
+        public void Pass()
+        {
+            Assert.True(true);
+        }
+
+        [Test]
+        public void Fail()
+        {
+            Assert.False(true);
+        }
+
+        [Test]
+        [Ignore("another time")]
+        public void Ignore()
+        {
+            Assert.True(false);
+        }
+    }
+}
---
+++ xamarin-phoneword/Phoneword_iOS/ViewController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+namespace Phoneword_iOS
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            string translatedNumber = "";
+
+            TranslateButton.TouchUpInside += (object sender, EventArgs e) => {
+                translatedNumber = PhoneTranslator.ToNumber(PhoneNumberText.Text);
+                DismissKeyboard();
+                if (translatedNumber == "")
+                {
+                    CallButton.SetTitle("Call", UIControlState.Normal);
+                    CallButton.Enabled = false;
+                }
+                else
+                {
+                    CallButton.SetTitle("Call " + translatedNumber, UIControlState.Normal);
+                    CallButton.Enabled = true;
+                }
+            };
+
+            CallButton.TouchUpInside += (object sender, EventArgs e) => {
+                var url = PhoneAppUrl(translatedNumber);
+                if (!UIApplication.SharedApplication.OpenUrl(url))
+                {
+                    var alert = PhoneNotSupported();
+                    PresentViewController(alert, true, null);
+                }
+            };
+        }
+
+        void DismissKeyboard()
+        {
+            PhoneNumberText.ResignFirstResponder();
+        }
+
+        NSUrl PhoneAppUrl(string translatedNumber)
+        {
+            return new NSUrl("tel:" + translatedNumber);
+        }
+
+        UIAlertController PhoneNotSupported()
+        {
+            UIAlertController alert = UIAlertController.Create(
+                "Not supported", 
+                "Scheme 'tel:' is not supported on this device", 
+                UIAlertControllerStyle.Alert
+            );
+            alert.AddAction(UIAlertAction.Create("Ok", UIAlertActionStyle.Default, null));
+            return alert;
+        }
+    }
+}
